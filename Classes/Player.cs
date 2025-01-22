285bb56bec136d8fad526e788ad70068cb3f52db
StephenieDugan <stepheniedugan617@gmail.com>
2025-01-22 09:48:36 -0700

    Start of Player Class
    variables, getters, and setters

--- Classes/Player.cs
+++ Classes/Player.cs
@@ -5,8 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MississippiMarbles.Classes
-{
+{ 
+	//Player Class { int points, string name, array saved_dice}
+
 	internal class Player
 	{ 
+		private String playerName;
+		private int points = 0;
+		private int[] savedDice;
+
+		Player(String playerName) { this.playerName = playerName; }
+
+		public String getPlayerName { get { return playerName}}
+		public int getPoints { get { return points;}}
+		public int[] getSavedDice { get { return savedDice;} }
+		public void setPoints(int points) { this.points = points;}
+		public void addToPot(int diceValue) {savedDice.Append(diceValue);}
 	}
 }
